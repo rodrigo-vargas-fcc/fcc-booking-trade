05e241300d10af07b6f9edd141cc2e697ba38218
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2016-07-20 23:57:40 +0000

    Fix port association

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ db.once('open', function() {
 
    routes(app, jwt, passport);
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
+   app.listen(port, function () {
+      console.log('Node.js listening on port ' + port + '...');
    });
 });
